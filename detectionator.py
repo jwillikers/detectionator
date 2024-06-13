79e5e13df72f6ed6c0771d9ba004b8c1d0d9dcc0
Jordan Williams <jordan@jwillikers.com>
2024-06-13 16:31:29 -0500

    Set up logger

--- detectionator.py
+++ detectionator.py
@@ -688,6 +688,12 @@ async def main():
     numeric_log_level = getattr(logging, args.log_level.upper(), None)
     if not isinstance(numeric_log_level, int):
         raise ValueError(f"Invalid log level: {args.log_level}")
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     logger.setLevel(numeric_log_level)
     logging.getLogger("picamera2").setLevel(numeric_log_level)
 
